cea8bd7ed2c5900d9810b733960c953cdc697ea2
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2025-01-08 12:56:43 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     proxy: {
       '/api': 'https://planet-fact-site-z05k.onrender.com',
