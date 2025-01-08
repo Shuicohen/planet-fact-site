f5295f49c8ca7668e0c00dfdddabd8db0286a17e
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2025-01-08 13:06:56 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+    { origin: ["https://planet-fact-site-1.onrender.com"],
+        methods: ["GET"], }
+    
+));
 app.use(express.json());
 
 // Routes
